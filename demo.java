40a1677969cdb95e3e9d88b530e13bed73cb60d1
satish <satishjmdav@gmail.com>
2025-06-13 15:05:51 +0530

    demo

---
+++ demo.java
@@ -0,0 +1,34 @@
+public class demo {
+
+    public static void main(String[] args) {
+        // int num = 10;
+        // if (num < 5) {
+        // System.out.println("TRUE");
+        // } else
+
+        // System.out.println("False");
+
+        // for loop
+        // for (int i = 0; i < 10; i++) {
+        // System.out.println(i);
+        // }
+
+        // int j = 0;
+        // while (j <= 5) {
+        // j++;
+        // System.out.println(j);
+        // // j++;
+        // }
+
+        // array
+        // int[] arr = new int[5];
+        // int[] nums = { 1, 2, 3, 4, 5 };
+        // System.out.println(nums[2]);
+
+        // string
+        String name = "Satish";
+        System.out.println(name.length());
+        System.out.println(name.charAt(3));
+        System.out.println(name.substring(1, 4));
+    }
+}
